68fd5fe134a288850f49e92341b3a2a63b09012d
rakibul <rakibul1810130@gmail.com>
2025-06-29 22:42:16 +0600

    Cash out form is succesfuly complete

---
+++ js/cash_out.js
@@ -0,0 +1,30 @@
+console.log('Cash out taka');
+
+document.getElementById('btn-cash-out').addEventListener('click',function(event){
+
+    event.preventDefault();
+    const  curentNumber2=document.getElementById('phone-number2').value;
+    currentBalance=parseFloat(curentNumber2);
+    const pin2=document.getElementById('pin-number2').value;
+    console.log(curentNumber2,pin2);
+       if(pin2==='1234'){
+        console.log('sucesfuly added');
+        const balance=document.getElementById('balance').innerText;
+        balanceNumber=parseFloat(balance);
+        console.log(balance);
+        const newBalance=balanceNumber-currentBalance;
+        console.log(newBalance);
+        document.getElementById('balance').innerText=newBalance;
+        
+        
+        
+
+    }
+    else{
+        console.log('please valid phone or pin number');
+        
+    }
+    
+    
+
+})
