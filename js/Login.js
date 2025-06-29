ab55974e228874149a89ca3c989dbabfe7373cfa
rakibul <rakibul1810130@gmail.com>
2025-06-29 16:47:50 +0600

    Login button press go to the another page

--- js/Login.js
+++ js/Login.js
@@ -1,1 +1,24 @@
-console.log('Add login ');
+
+ document.getElementById('btn-login').addEventListener('click',function(event){
+    // const phone_number = document.getElementById('phone-number'
+    const number=document.getElementById('phone-number').value;
+    const pin =document.getElementById('pin-number').value;
+    event.preventDefault();
+    console.log(number,pin );
+
+    if(number==='017' && pin==='1234'){
+        console.log('Valid number and pin');
+        window.location.href='/home.html';
+        
+        
+    }
+    else{
+        console.log('invalid phone number or pin');
+        
+    }
+    
+ })
+
+
+ 
+
