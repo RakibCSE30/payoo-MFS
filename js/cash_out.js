4d8253a1a9230e879b171587ade14558742b24af
rakibul <rakibul1810130@gmail.com>
2025-07-01 06:34:29 +0600

    Cash out using function is better performance

--- js/cash_out.js
+++ js/cash_out.js
@@ -1,30 +1,24 @@
- 
-
-document.getElementById('btn-cash-out').addEventListener('click',function(event){
-
+document
+  .getElementById("btn-cash-out")
+  .addEventListener("click", function (event) {
     event.preventDefault();
-    const  curentNumber2=document.getElementById('phone-number2').value;
-    currentBalance=parseFloat(curentNumber2);
-    const pin2=document.getElementById('pin-number2').value;
-    console.log(curentNumber2,pin2);
-       if(pin2==='1234'){
-        console.log('sucesfuly added');
-        const balance=document.getElementById('balance').innerText;
-        balanceNumber=parseFloat(balance);
-        console.log(balance);
-        const newBalance=balanceNumber-currentBalance;
-        console.log(newBalance);
-        document.getElementById('balance').innerText=newBalance;
-        
-        
-        
 
+    const curentNumber2 =getInputFieldValueById('phone-number2');
+    //  document.getElementById("phone-number2").value;
+    // currentBalance = parseFloat(curentNumber2);
+    const pin2 = getInputFieldValueById('pin-number2');
+    // document.getElementById("pin-number2").value;
+    console.log(curentNumber2, pin2);
+    if (pin2 === 1234) {
+      console.log("sucesfuly added");
+      const balance = getInputTextValueById('balances')
+    //   document.getElementById("balance").innerText;
+    //   balanceNumber = parseFloat(balance);
+      console.log(balance);
+      const newBalance = balance - curentNumber2;
+      console.log(newBalance);
+      document.getElementById("balances").innerText = newBalance;
+    } else {
+      console.log("please valid phone or pin number");
     }
-    else{
-        console.log('please valid phone or pin number');
-        
-    }
-    
-    
-
-})
+  });
